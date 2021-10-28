b99c7e61306c548a7bd82753c7b97f39bb9460df
Elora Elbert <82468024+eloraelbert@users.noreply.github.com>
2021-10-28 21:56:38 +0000

    Finished code. Now lists dog breeds and poodle subbreeds nicely. Current version works.

--- main.py
+++ main.py
@@ -7,4 +7,22 @@ query = ""
 url = url_host + endpoint # query
 
 response = requests.get(url)
-print(response.json())
+breeds = response.json()["message"]
+
+# get a list of all breeds from API call
+all_breeds = breeds.keys()
+
+# loop and print out each key
+
+for breed in all_breeds:
+  print(breed)
+
+# Get a list of all subbreeds w at least 3 sub-breeds
+# but NOT bulldogs
+# Examples include: poodles, spaniels, retrievers, terriers, mastiffs, setters exit
+
+#Display all poodle subbreeds
+poodle_subbreeds = breeds["poodle"]
+input("\nPress enter to get all poodle subbreeds")
+for sub in poodle_subbreeds:
+  print(sub)
